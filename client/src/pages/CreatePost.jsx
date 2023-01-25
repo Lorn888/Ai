7d313e72e1d1c20e0833c70dc899d5644f4847d9
Patryk <patrykokpara@hotmail.com>
2023-01-25 22:59:46 +0000

    Corrections

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { preview } from '../assets'
+import { getRandomPrompt } from '../utils'
+import { FormField, Loader } from '../components'
 
 const CreatePost = () => {
+  const navigate = useNavigate
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false)
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7x1' mx-auto>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>
+          Create</h1>
+        <p className='mt-2 text-[#666e75] text=[16px] max-w[500px]'>Creative and visually stunning images trough DALL-E AI and share them with community</p>
+      </div>
+ 
+    </section>
   )
 }
 
