832efb79c300e5f14702f8958d4668712f00e23f
Patryk <patrykokpara@hotmail.com>
2023-01-26 19:10:33 +0000

    Enabled forms functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -22,11 +22,12 @@ const CreatePost = () => {
   }
 
   const handleChange = (e) => {
-
+    setForm({ ...form, [e.target.name]: e.target.value })
   }
 
   const handleSurpriseMe = () => {
-
+    const randomPrompt = getRandomPrompt(form.prompt)
+    setForm({ ...form, prompt: randomPrompt})
   }
   return (
     <section className='max-w-7x1' mx-auto>
